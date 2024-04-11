0443658f35cd34e32f3b91d7b4198619c34e11db
diogoaav <116834699+diogoaav@users.noreply.github.com>
2024-04-11 12:48:18 -0400

    Update metrics.sh

--- metrics.sh
+++ metrics.sh
@@ -29,45 +29,4 @@ for droplet_id in $(jq -r '.droplets[] | .id' data/droplets.json); do
     "https://api.digitalocean.com/v2/monitoring/metrics/droplet/bandwidth" \
     | jq '.' > "data/bandwidth-inbound-${droplet_id}.json"
 
-  echo "Looking up outbound bandwidth for droplet ${droplet_id}"
-  curl --silent --show-error --fail \
-    --header "Accept: application/json" \
-    --header "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
-    --url-query "host_id=${droplet_id}" \
-    --url-query "interface=public" \
-    --url-query "direction=outbound" \
-    --url-query "start=${start_ts}" \
-    --url-query "end=${end_ts}" \
-    "https://api.digitalocean.com/v2/monitoring/metrics/droplet/bandwidth" \
-    | jq '.' > "data/bandwidth-outbound-${droplet_id}.json"
-
-  echo "Looking up cpu for droplet ${droplet_id}"
-  curl --silent --show-error --fail \
-    --header "Accept: application/json" \
-    --header "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
-    --url-query "host_id=${droplet_id}" \
-    --url-query "start=${start_ts}" \
-    --url-query "end=${end_ts}" \
-    "https://api.digitalocean.com/v2/monitoring/metrics/droplet/cpu" \
-    | jq '.' > "data/cpu-${droplet_id}.json"
-
-  echo "Looking up free memory for droplet ${droplet_id}"
-  curl --silent --show-error --fail \
-    --header "Accept: application/json" \
-    --header "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
-    --url-query "host_id=${droplet_id}" \
-    --url-query "start=${start_ts}" \
-    --url-query "end=${end_ts}" \
-    "https://api.digitalocean.com/v2/monitoring/metrics/droplet/memory_free" \
-    | jq '.' > "data/memory-free-${droplet_id}.json"
-
-  echo "Looking up total memory for droplet ${droplet_id}"
-  curl --silent --show-error --fail \
-    --header "Accept: application/json" \
-    --header "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
-    --url-query "host_id=${droplet_id}" \
-    --url-query "start=${start_ts}" \
-    --url-query "end=${end_ts}" \
-    "https://api.digitalocean.com/v2/monitoring/metrics/droplet/memory_total" \
-    | jq '.' > "data/memory-total-${droplet_id}.json"
 done
